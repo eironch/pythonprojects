02130649d01fa8b1bf839447051432c7c6a21e59
ficuno <eldritchgallows@gmail.com>
2023-03-26 15:55:13 +0800

    image viewer project added

---
+++ image_viewer/image_viewer.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+def main():
+    root = Tk()
+    root.title("Image Viewer")
+    root.iconbitmap('picture.ico')
+    global image_index
+    image_index = 0
+
+    img1 = ImageTk.PhotoImage(Image.open("images/1.png").resize((512, 512), Image.LANCZOS))
+    img2 = ImageTk.PhotoImage(Image.open("images/2.gif").resize((512, 512), Image.LANCZOS))
+    img3 = ImageTk.PhotoImage(Image.open("images/3.png").resize((512, 512), Image.LANCZOS))
+    img4 = ImageTk.PhotoImage(Image.open("images/4.png").resize((512, 512), Image.LANCZOS))
+    img5 = ImageTk.PhotoImage(Image.open("images/5.png").resize((512, 512), Image.LANCZOS))
+    img6 = ImageTk.PhotoImage(Image.open("images/6.png").resize((512, 512), Image.LANCZOS))
+    img7 = ImageTk.PhotoImage(Image.open("images/7.gif").resize((512, 512), Image.LANCZOS))
+    img8 = ImageTk.PhotoImage(Image.open("images/8.png").resize((512, 512), Image.LANCZOS))
+    img9 = ImageTk.PhotoImage(Image.open("images/9.png").resize((512, 512), Image.LANCZOS))
+    img10 = ImageTk.PhotoImage(Image.open("images/10.png").resize((512, 512), Image.LANCZOS))
+    img11 = ImageTk.PhotoImage(Image.open("images/11.png").resize((512, 512), Image.LANCZOS))
+    img12 = ImageTk.PhotoImage(Image.open("images/12.png").resize((512, 512), Image.LANCZOS))
+
+    image_list = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12]
+
+    global label
+
+    label = Label(image=image_list[image_index])
+    label.grid(row=0, column=0, columnspan=3)
+
+    def button_backwards(len):
+        global image_index
+        global label
+
+        if image_index != 0:
+            image_index -= 1
+        else:
+            image_index = len
+
+        label.grid_forget()
+        label = Label(image=image_list[image_index - 1])
+        label.grid(row=0, column=0, columnspan=3)
+
+    def button_forwards(len):
+        global image_index
+        global label
+
+        if image_index != len:
+            image_index += 1
+        else:
+            image_index = 0
+
+        label.grid_forget()
+        label = Label(image=image_list[image_index - 1])
+        label.grid(row=0, column=0, columnspan=3)
+
+    button_back = Button(root, text="<<", command=lambda: button_backwards(len(image_list)))
+    button_quit = Button(root, text="Quit", command=root.quit)
+    button_forward = Button(root, text=">>", command=lambda: button_forwards(len(image_list)))
+
+    button_back.grid(row=1, column=0)
+    button_quit.grid(row=1, column=1)
+    button_forward.grid(row=1, column=2)
+
+    root.mainloop()
+
+
+if __name__== "__main__":
+    main()
