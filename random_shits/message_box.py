995b057a13333cfafd0207bf3c71d2a3e710c409
ficuno <eldritchgallows@gmail.com>
2023-03-31 22:06:01 +0800

    message box script created

---
+++ random_shits/message_box.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import messagebox
+
+def main():
+    root = Tk()
+
+    root.title('Messag Boxes')
+
+    # showinfo, showwarning, showerror, askquestion, askokcancel, askyesno
+    def popup():
+        if not messagebox.askyesno("Message Box", "Do you like me?"):
+            popup()
+
+        else:
+            messagebox.showinfo("Message Box", "I like you too!")
+
+
+    popup()
+    quit()
+    mainloop()
+
+
+if __name__ == "__main__":
+    main()
