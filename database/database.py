27dc45517cdc3f2dbe2c7627276e309e0c988db1
ficuno <eldritchgallows@gmail.com>
2023-04-07 16:06:23 +0800

    added delete record func

--- database/database.py
+++ database/database.py
@@ -25,7 +25,7 @@ def main():
 
     # Create text boxes
     e_f_name = Entry(root, width=30)
-    e_f_name.grid(row=0, column=1, padx=20)
+    e_f_name.grid(row=0, column=1, padx=20, pady=(10,0))
     e_l_name = Entry(root, width=30)
     e_l_name.grid(row=1, column=1, padx=20)
     e_address = Entry(root, width=30)
@@ -36,10 +36,12 @@ def main():
     e_province.grid(row=4, column=1, padx=20)
     e_zipcode = Entry(root, width=30)
     e_zipcode.grid(row=5, column=1, padx=20)
+    e_delete = Entry(root, width=30)
+    e_delete.grid(row=8, column=1, padx=20)
 
     # Create text box labels
     f_name_label = Label(root, text="First Name: ")
-    f_name_label.grid(row=0, column=0)
+    f_name_label.grid(row=0, column=0, pady=(10,0))
     l_name_label = Label(root, text="Last Name: ")
     l_name_label.grid(row=1, column=0)
     address_label = Label(root, text="Address: ")
@@ -50,9 +52,30 @@ def main():
     province_label.grid(row=4, column=0)
     zipcode_label = Label(root, text="Zipcode: ")
     zipcode_label.grid(row=5, column=0)
+    delete_label = Label(root, text="Delete Record ID: ")
+    delete_label.grid(row=8, column=0)
+
+    # Create function to delete record
+    def delete():
+        # Create a database or connect to one
+        conn = sqlite3.connect('address_book.db')
+        # Create cursor
+        cursor = conn.cursor()
+
+        # Delete a record
+        cursor.execute("DELETE from addresses WHERE oid= " + e_delete.get())
+
+        # Commit changes
+        conn.commit()
+
+        # Close connection
+        conn.close()
 
     # Create submit function for database
     def submit():
+        if len(e_f_name.get()) == 0:
+            return
+
         # Create a database or connect to one
         conn = sqlite3.connect('address_book.db')
 
@@ -95,12 +118,11 @@ def main():
         for item in range(len(records)):
             for record in records[item]:
                 if record == records[item][0]:
-                    print_records += f"Record #{item}" + "\n"
+                    print_records += f"Record #{records[item][6]}" + "\n"
 
                 if record != records[item][6]:
                     print_records += str(record) + "\n"
                 elif item != len(records) - 1:
-                    print (item, len(records) - 1)
                     print_records += "\n"
 
         query_label = Label(top, text=print_records)
@@ -133,6 +155,11 @@ def main():
     # Query button
     query = Button(root, text="Show Records", width=50, command=query)
     query.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+
+    # Create a delete button
+
+    delete = Button(root, text="Delete Record", width=50, command=delete)
+    delete.grid(row=9, column=0, columnspan=2, padx=10, pady=10)
 
     # Commit changes
     conn.commit()
