edea5426b6c595393433e03c3e9d8c9c331706bc
ficuno <eldritchgallows@gmail.com>
2023-04-15 23:16:10 +0800

    added header

--- notes_app/notes_app.py
+++ notes_app/notes_app.py
@@ -1,14 +1,22 @@
 from tkinter import *
 
-
-def main():
-    global root
-    root = Tk()
+def note_taking(root):
     root.title('For Note Taking')
 
-    e_note = Entry(root, width=30, bd=0)
-    e_note.grid()
+    header = Label(root, text="For Note Taking", font=("Helvetica", 20, "bold"))
+    header.grid(row=0, column=0, padx=30, pady=(30, 10))
 
+    global notes_stored
+
+    e_note = Entry(root, width=30, bd=0, bg="light yellow")
+    e_note.grid(row=1, column=0)
+
+    #globals()["line_" + str(line)]
+
+def main():
+    root = Tk()
+
+    note_taking(root)
     root.mainloop()
 
 if __name__ == "__main__":
