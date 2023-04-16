0fb7d33653da0eb256925b145a852379a7c75fea
ficuno <eldritchgallows@gmail.com>
2023-04-16 12:34:53 +0800

    added more note entries

--- notes_app/notes_app.py
+++ notes_app/notes_app.py
@@ -3,15 +3,19 @@ from tkinter import *
 def note_taking(root):
     root.title('For Note Taking')
 
+    def enter(*args):
+        print("yes")
+        root.focus()
+
     header = Label(root, text="For Note Taking", font=("Helvetica", 20, "bold"))
-    header.grid(row=0, column=0, padx=30, pady=(30, 10))
+    header.grid(row=0, column=0, padx=30, pady=(30, 10), columnspan=10)
 
-    global notes_stored
-
-    e_note = Entry(root, width=30, bd=0, bg="light yellow")
-    e_note.grid(row=1, column=0)
-
-    #globals()["line_" + str(line)]
+    for x in range(1,5):
+        globals()["note" + str(x)] = Label(root, text="Note " + str(x) + ": ", font=("Helvetica", 10, "bold"))
+        globals()["note" + str(x)].grid(row=x, column=0, padx=(30,0))
+        globals()["e_note" + str(x)] = Entry(root, width=30, bd=0, bg="light yellow")
+        globals()["e_note" + str(x)].grid(row=x, column=1, padx=(0, 35))
+        globals()["e_note" + str(x)].bind('<Return>', enter)
 
 def main():
     root = Tk()
