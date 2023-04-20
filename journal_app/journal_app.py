82be316e1adf476aaa61e548eebc92bf6be7b8fa
ficuno <eldritchgallows@gmail.com>
2023-04-20 22:28:32 +0800

    updated script

--- journal_app/journal_app.py
+++ journal_app/journal_app.py
@@ -2,11 +2,11 @@ from tkinter import *
 
 def journal(root):
     root.title('Your Journal')
-
+    header = Label(root, text="Your Journal", font=("Helvetica", 20, "bold"))
+    header.grid(row=0, column=0, columnspan=10)
 def main():
     global root
     root = Tk()
-
 
     journal(root)
     root.mainloop()
