f40b7dc8d6dca01645e52d4ed09d4ef893736f1d
ficuno <eldritchgallows@gmail.com>
2023-04-21 22:16:17 +0800

    added weekdays

--- journal_app/journal_app.py
+++ journal_app/journal_app.py
@@ -3,7 +3,22 @@ from tkinter import *
 def journal(root):
     root.title('Your Journal')
     header = Label(root, text="Your Journal", font=("Helvetica", 20, "bold"))
-    header.grid(row=0, column=0, columnspan=10)
+    header.grid(row=0, column=0, columnspan=10, padx=30, pady=(30,10))
+
+    weekday_list = ["Sunday",
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday"
+                    ]
+
+    for x in range(7):
+        globals()["weekday_" + str(x + 1)] = Label(root, text=weekday_list[x], font=("Helvetica", 10))
+        globals()["weekday_" + str(x + 1)].grid(row = 1, column = x, padx=30)
+
+
 def main():
     global root
     root = Tk()
