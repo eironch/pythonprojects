5e17177813f4094886e57da17e1717411d55ef67
ficuno <eldritchgallows@gmail.com>
2023-04-23 23:47:53 +0800

    updated script

--- journal_app/journal_app.py
+++ journal_app/journal_app.py
@@ -64,37 +64,6 @@ def find_month_name():
         case 12:
             return "December"
 
-
-def find_days_in_month():
-    match month:
-        case 1:
-            return 31
-        case 2:
-            if year % 4 != 0:
-                return 28
-            else:
-                return 29
-        case 3:
-            return 31
-        case 4:
-            return 30
-        case 5:
-            return 31
-        case 6:
-            return 30
-        case 7:
-            return 31
-        case 8:
-            return 31
-        case 9:
-            return 30
-        case 10:
-            return 31
-        case 11:
-            return 30
-        case 12:
-            return 31
-
 def journal(root):
     root.title('Your Journal')
 
