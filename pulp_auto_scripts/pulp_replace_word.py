4e92487e52df698e23ff65e407d9ab2a2fc07495
ficuno <eldritchgallows@gmail.com>
2023-07-17 13:08:54 +0800

    updated code

--- pulp_auto_scripts/pulp_replace_word.py
+++ pulp_auto_scripts/pulp_replace_word.py
@@ -1,12 +1,22 @@
 import sys
 
+
 def main():
-    word_to_be_replaced = input("Input Word to be Replaced: ")
-    word_to_replace = input("Input Word to Replace: ")
+    word_to_be_replaced_list = []
+    word_to_replace_list = []
+
+    for i in range(int(input("Input number of words to replace: "))):
+        word_to_be_replaced_list.append(input("Input word to be replaced: "))
+        word_to_replace_list.append(input("Input word to replace: "))
+
     print("Paste Code, then CTRL + D to end Input.")
     code = "\n" + ''.join(sys.stdin.readlines())
     print("\nResult:\n")
-    print(code.replace(word_to_be_replaced, word_to_replace))
+
+    for i in range(len(word_to_be_replaced_list)):
+        code = code.replace(word_to_be_replaced_list[i], word_to_replace_list[i])
+
+    print(code)
 
 
 if __name__ == "__main__":
